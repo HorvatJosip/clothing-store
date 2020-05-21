6dca3047ebb6e63f4403de624bd9c5eb90062701
jhorva1 <jhorva1@racunarstvo.hr>
2020-05-21 21:33:14 +0200

    Added header with navigation bar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
+import Header from './components/Header/Header';
 import HomePage from './pages/HomePage/HomePage';
 import ShopPage from './pages/ShopPage/ShopPage';
 
@@ -8,6 +9,7 @@ import './App.css';
 
 const App = () => (
   <div>
+    <Header />
     <Switch>
       <Route exact path='/' component={HomePage} />
       <Route path='/shop' component={ShopPage} />
