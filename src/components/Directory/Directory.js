556e6d7de74b5b451f48c81351fcebbf6a42704d
jhorva1 <jhorva1@racunarstvo.hr>
2020-05-21 20:44:51 +0200

    Added routing for MenuItem component

--- src/components/Directory/Directory.js
+++ src/components/Directory/Directory.js
@@ -13,28 +13,33 @@ class Directory extends Component {
           id: 1,
           title: 'hats',
           imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+          linkUrl: 'hats',
         },
         {
           id: 2,
           title: 'jackets',
           imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+          linkUrl: 'jackets',
         },
         {
           id: 3,
           title: 'sneakers',
           imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+          linkUrl: 'sneakers',
         },
         {
           id: 4,
           title: 'womens',
           imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
           size: 'large',
+          linkUrl: 'womens',
         },
         {
           id: 5,
           title: 'mens',
           imageUrl: 'https://i.ibb.co/R70vBrQ/mens.png',
           size: 'large',
+          linkUrl: 'mens',
         },
       ],
     };
@@ -42,8 +47,8 @@ class Directory extends Component {
   render() {
     return (
       <div className='directory-menu'>
-        {this.state.sections.map(({ id, title, imageUrl, size }) => (
-          <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+        {this.state.sections.map(({ id, ...otherSectionProps }) => (
+          <MenuItem key={id} {...otherSectionProps} />
         ))}
       </div>
     );
