0afe3d6e9882c5a0ab059650bf563f58002e4604
jhorva1 <jhorva1@racunarstvo.hr>
2020-05-31 21:27:30 +0200

    Implement quantity change on checkout page

--- src/redux/cart/CartActions.js
+++ src/redux/cart/CartActions.js
@@ -7,9 +7,21 @@ export const addItem = item => ({
   payload: item,
 });
 
+export const removeItem = item => ({
+  type: NAMES.REMOVE_ITEM,
+  payload: item,
+});
+
+export const clearItemFromCart = item => ({
+  type: NAMES.CLEAR_ITEM_FROM_CART,
+  payload: item,
+});
+
 const NAMES = {
   TOGGLE_CART_VISIBILITY: 'TOGGLE_CART_VISIBILITY',
   ADD_ITEM: 'ADD_ITEM',
+  REMOVE_ITEM: 'REMOVE_ITEM',
+  CLEAR_ITEM_FROM_CART: 'CLEAR_ITEM_FROM_CART',
 };
 
 export default NAMES;
--- src/redux/cart/CartReducer.js
+++ src/redux/cart/CartReducer.js
@@ -1,6 +1,6 @@
 import ActionNames from './CartActions';
 
-import { addItemToCart } from './CartUtils';
+import { addItemToCart, removeItemFromCart } from './CartUtils';
 
 const INITIAL_STATE = {
   hidden: true,
@@ -19,6 +19,20 @@ const CartReducer = (state = INITIAL_STATE, action) => {
       return {
         ...state,
         cartItems: addItemToCart(state.cartItems, action.payload),
+      };
+
+    case ActionNames.REMOVE_ITEM:
+      return {
+        ...state,
+        cartItems: removeItemFromCart(state.cartItems, action.payload),
+      };
+
+    case ActionNames.CLEAR_ITEM_FROM_CART:
+      return {
+        ...state,
+        cartItems: state.cartItems.filter(
+          cartItem => cartItem.id !== action.payload.id
+        ),
       };
 
     default:
